db13fa5606822af4b06800b1a09ef82f31c8f1b8
Luis Guiraldelli <guiraldelli@gmail.com>
2019-03-05 22:42:54 -0300

    ADD endpoint clientes/{id} available

---
+++ src/main/java/com/guirald/cursomc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package com.guirald.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guirald.cursomc.domain.Categoria;
+import com.guirald.cursomc.domain.Cliente;
+import com.guirald.cursomc.repositories.ClienteRepository;
+import com.guirald.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id);
+		return cliente.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado. Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+}
