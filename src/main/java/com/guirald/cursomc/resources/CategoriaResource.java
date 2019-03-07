e2827b4d73ea819dc8800358f8e949e3b1105866
Luis Guiraldelli <guiraldelli@gmail.com>
2019-03-06 23:08:20 -0300

    ADD categoria with POST Call

--- src/main/java/com/guirald/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/guirald/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.guirald.cursomc.resources;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.guirald.cursomc.domain.Categoria;
 import com.guirald.cursomc.services.CategoriaService;
@@ -24,5 +27,14 @@ public class CategoriaResource {
 
 		Optional<Categoria> categoria = Optional.ofNullable(categoriaService.buscar(id));
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+		categoria = categoriaService.insert(categoria);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(categoria.getId())
+				.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
