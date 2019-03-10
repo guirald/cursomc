fd50a722c25da5ac7ea40d58061e6642e6c78437
Luis Guiraldelli <guiraldelli@gmail.com>
2019-03-10 15:01:42 -0300

    DELETING ONE CATEGORY AND NEW DATAINTEGRITYEXCEPTION HANDLER

--- src/main/java/com/guirald/cursomc/services/CategoriaService.java
+++ src/main/java/com/guirald/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.guirald.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.guirald.cursomc.domain.Categoria;
 import com.guirald.cursomc.repositories.CategoriaRepository;
+import com.guirald.cursomc.services.exceptions.DataIntegrityException;
 import com.guirald.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,15 @@ public class CategoriaService {
 	public Categoria update(Categoria categoria) {
 		find(categoria.getId());
 		return categoriaRepository.save(categoria);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			categoriaRepository.deleteById(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto(s) vinculado(s)");
+		}
 	}
 
 }
