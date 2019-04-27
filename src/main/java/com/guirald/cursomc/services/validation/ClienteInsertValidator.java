261880923986ebf2fd1aef59b8eea6fc36d5ea22
Luis Guiraldelli <guiraldelli@gmail.com>
2019-04-27 13:18:55 -0300

    MOD email unique custom validation

--- src/main/java/com/guirald/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/guirald/cursomc/services/validation/ClienteInsertValidator.java
@@ -2,15 +2,22 @@ package com.guirald.cursomc.services.validation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import javax.validation.ConstraintValidator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.guirald.cursomc.domain.enums.TipoCliente;
 import com.guirald.cursomc.dto.ClienteNewDTO;
+import com.guirald.cursomc.repositories.ClienteRepository;
 import com.guirald.cursomc.resources.exception.FieldMessage;
 import com.guirald.cursomc.services.validation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
 	@Override
 	public void initialize(ClienteInsert constraintAnnotation) {};
@@ -26,6 +33,8 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
 		}
+		
+		Optional.ofNullable(clienteRepository.findByEmail(objDto.getEmail())).ifPresent(c -> list.add(new FieldMessage("email", "E-mail já existente")));
 		
 		for (FieldMessage e : list) {
 			// transportar os erros personalizados para a lista de erros do framework
