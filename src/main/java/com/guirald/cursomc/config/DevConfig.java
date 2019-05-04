8148911a171c4b8812a8cfa534fb64089395e770
Luis Guiraldelli <luis.guiraldelli@luizalabs.com>
2019-05-04 14:55:44 -0300

    ADD SmtpEmailService with Google Email Service

--- src/main/java/com/guirald/cursomc/config/DevConfig.java
+++ src/main/java/com/guirald/cursomc/config/DevConfig.java
@@ -9,24 +9,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.guirald.cursomc.services.DBService;
+import com.guirald.cursomc.services.EmailService;
+import com.guirald.cursomc.services.SmtpEmailService;
 
 @Configuration
 @Profile("dev")
 public class DevConfig {
-	
+
 	@Autowired
 	private DBService dbService;
-	
+
 	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
-	
+
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		if (!"create".equals(strategy))
 			return false;
-		
+
 		dbService.instantiateTestDatabase();
 		return true;
+	}
+
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
 	}
 
 }
