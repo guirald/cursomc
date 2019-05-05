eadf2af5ccd16bba98f7f20543e06f388c4d842b
Luis Guiraldelli <luis.guiraldelli@luizalabs.com>
2019-05-05 16:15:31 -0300

    ADD authentication management and jwt token generation

---
+++ src/main/java/com/guirald/cursomc/dto/CredenciaisDTO.java
@@ -0,0 +1,31 @@
+package com.guirald.cursomc.dto;
+
+import java.io.Serializable;
+
+public class CredenciaisDTO implements Serializable {
+
+	private static final long serialVersionUID = 4194834062142182829L;
+
+	private String email;
+	private String senha;
+
+	public CredenciaisDTO() {
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
